38b3e3d71e4c06ebff893c9b6353eff31f1d250e
alice-ibba5 <135819129+alice-ibba5@users.noreply.github.com>
2024-01-10 01:11:10 +0100

    aggiunta rotta get users

--- src/userRouter.js
+++ src/userRouter.js
@@ -1,5 +1,16 @@
 import express from "express";
+import { User } from "./models/users.js";
 
 const userRouter = express.Router();
+
+userRouter.get("/", async (req, res, next) => {
+  //ritorna tutti gli autori
+  try {
+    const users = await User.find({});
+    res.json(users);
+  } catch (error) {
+    next(error);
+  }
+});
 
 export default userRouter;
