35103075b8b9ac8ea6999b0591da820d9cb786d9
alice-ibba5 <135819129+alice-ibba5@users.noreply.github.com>
2024-01-20 02:10:51 +0100

    up

--- src/middlewares/google.js
+++ src/middlewares/google.js
@@ -23,42 +23,41 @@ const googleStrategy = new GoogleStrategy(
         email: profile.emails[0].value,
         avatar: profile.photos[0].value,
       });
-    }
 
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      secure: false,
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASSWORD2,
-      },
-      tls: {
-        rejectUnauthorized: false,
-      },
-    });
-    // Invia l'email di benvenuto
-    try {
-      const mailOptions = {
-        from: "gdlove.wordsforthesoul@gmail.com",
-        to: user.email,
-        subject: "Email di benvenuto!",
-        text: `Email di benvenuto a ${user.name} ${user.surname}. 
+      const transporter = nodemailer.createTransport({
+        service: "gmail",
+        secure: false,
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASSWORD2,
+        },
+        tls: {
+          rejectUnauthorized: false,
+        },
+      });
+      // Invia l'email di benvenuto
+      try {
+        const mailOptions = {
+          from: "gdlove.wordsforthesoul@gmail.com",
+          to: user.email,
+          subject: "Email di benvenuto!",
+          text: `Email di benvenuto a ${user.name} ${user.surname}. 
       BenvenutO su ✨GDLove🔮 - il luogo ideale per gli amanti dei libri che
       desiderano connettersi, esplorare nuove letture e partecipare a
       entusiasmanti discussioni letterarie. Siamo un team appassionato
       di lettori e sviluppatori che si sono uniti per creare
       un'esperienza coinvolgente e interattiva per tutti coloro che
       amano immergersi nel mondo della letteratura.`,
-      };
+        };
 
-      await transporter.sendMail(mailOptions);
-      console.log("Email di benvenuto inviata con successo");
-    } catch (error) {
-      console.error("Errore durante l'invio dell'email di benvenuto:", error);
-      // Gestisci l'errore in modo appropriato, ad esempio, restituisci un errore al chiamante
-      return cb(error, null);
+        await transporter.sendMail(mailOptions);
+        console.log("Email di benvenuto inviata con successo");
+      } catch (error) {
+        console.error("Errore durante l'invio dell'email di benvenuto:", error);
+        // Gestisci l'errore in modo appropriato, ad esempio, restituisci un errore al chiamante
+        return cb(error, null);
+      }
     }
-
     cb(null, user);
   }
 );
